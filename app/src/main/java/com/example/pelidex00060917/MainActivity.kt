2a47f9a6d25dfa32ea35e61aa797680f5c5f65a5
CesarRosales16 <00060917@uca.edu.sv>
2019-04-05 11:07:15 -0600

    se acabo el tiempo del labo

--- app/src/main/java/com/example/pelidex00060917/MainActivity.kt
+++ app/src/main/java/com/example/pelidex00060917/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.pelidex00060917
 
+import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,5 +31,34 @@ class MainActivity : AppCompatActivity() {
             layoutManager = viewManager
             adapter = movieAdapter
         }
+    }
+
+    fun addMovieToList(movie: Movie) {
+        movieList.add(movie)
+        movieAdapter.changeList(movieList)
+        Log.d("Number", movieList.size.toString())
+    }
+
+    private inner class FetchMovie : AsyncTask<String, Void, String>() {
+        override fun doInBackground(vararg params: String): String {
+            if (params.isNullOrEmpty()) return ""
+
+            val movieName = params[0]
+            val movieUrl = NetworkUtils().buildSearchUrl(movieName)
+
+            return try {
+                NetworkUtils().getResponseFromHttpUrl(movieUrl)
+            } catch (e: IOException) {
+                ""
+            }
+        }
+
+        override fun onPostExecute(movieInfo: String) {
+            super.onPostExecute(movieInfo)
+            if(!movieInfo.isEmpty()) {
+                //val movieJson = JSONObject
+            }
+        }
+
     }
 }
